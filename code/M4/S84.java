c996add84d140a3e65a14aaf2b33cd380e94c19f
snowcying <347025712@qq.com>
2023-04-02 21:57:37 +0800

    单调栈，没有做出来

---
+++ code/M4/S84.java
@@ -0,0 +1,12 @@
+package code.M4;
+
+public class S84 {
+    public static int largestRectangleArea(int[] heights) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] heights = {2, 1, 5, 6, 2, 3};
+        System.out.println(largestRectangleArea(heights));
+    }
+}
